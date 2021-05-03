caafc8ce51e73460e7ed259c8e50baf93e3dd4a0
Tiyala Al-Totep <ttestroven@gmail.com>
2021-05-03 16:51:33 +0300

    обновила 5ю задачу

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,5 @@ function truncate(str, maxlength) {
   if (str.length > maxlength) {
     return str.substring(0, maxlength-1) + dots
   }
-    return str[0].toUpperCase() + str.substring(1)
+    return str
 }
